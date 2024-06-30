f04e4e3ea90700d3b5192cca52d92fda1301471a
feroline <anacarolina.rrocha@gmail.com>
2024-06-30 17:18:21 -0300

    verificar visibilidade e link do banner

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+beforeEach(() => {
+    cy.visit('/')
+ })
+
+describe('Home', () => {
+    describe('Banner', () => {
+
+        it('Verificar visibilidade e Link Banner', () => {
+            cy.get('div.home-banner').should('be.visible');
+            cy.get('div.home-banner a[href="https://www.toolsqa.com/selenium-training/"]').should('be.visible')
+            cy.get('div.home-banner a[href="https://www.toolsqa.com/selenium-training/"]')
+            .click().then(() => {
+                cy.request('eq', 'https://www.toolsqa.com/selenium-training/').its('status').should('eq', 200)
+            })
+        })
+        
+    })
+    
+})
