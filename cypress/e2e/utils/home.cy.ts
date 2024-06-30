80e05ef8bbce8f32302d5ea63522a27ee3ea69a6
feroline <anacarolina.rrocha@gmail.com>
2024-06-30 19:30:07 -0300

    retornando a constante após teste de constructor

--- cypress/e2e/utils/home.cy.ts
+++ cypress/e2e/utils/home.cy.ts
@@ -1,27 +1,27 @@
-import HomePage from "../../pages/HomePage"
+import HomePage from "../../pages/HomePage";
 const Home = new HomePage();
 
 beforeEach(() => {
-    cy.visit('/')
- })
+  cy.visit("/");
+});
 
-describe('Home', () => {
-    describe('Visibilidade', () => {
+describe("Home", () => {
+  describe("Visibilidade", () => {
+    it("Banner", () => {
+      Home.getBanner().should("be.visible");
+    });
+  });
 
-        it('Banner', () => {
-           Home.getBanner().should('be.visible');
-        })
-        
-    })
-
-    describe.only('Redirecionamento', () => {
-        it.only('Banner', () => {
-            Home.getLinkBanner()
-                .should('exist')
-                .click().then(() => {
-                    cy.request('eq', Home.getStringLinkBanner).its('status').should('eq', 200)
-                })
-        })
-    })
-    
-})
+  describe("Redirecionamento", () => {
+    it("Banner", () => {
+      Home.getLinkBanner()
+        .should("exist")
+        .click()
+        .then(() => {
+          cy.request("eq", Home.getStringLinkBanner)
+            .its("status")
+            .should("eq", 200);
+        });
+    });
+  });
+});
--- cypress/pages/HomePage.ts
+++ cypress/pages/HomePage.ts
@@ -1,22 +1,17 @@
-const linkBanner = 'https://www.toolsqa.com/selenium-training/'; 
-const selectorBanner = 'div.home-banner';
-
+const linkBanner = "https://www.toolsqa.com/selenium-training/";
+const selectorBanner = "div.home-banner";
 class HomePage {
+  getBanner() {
+    return cy.get(selectorBanner);
+  }
 
+  getLinkBanner() {
+    return cy.get(`${selectorBanner} a[href="${linkBanner}"]`);
+  }
 
-    getBanner() {
-        return cy.get(selectorBanner)
-    }
-
-    getLinkBanner() {
-        return cy.get(`${selectorBanner} a[href="${linkBanner}"]`)
-    }
-
-    getStringLinkBanner() {
-        return linkBanner; 
-    }
-
-
+  getStringLinkBanner() {
+    return linkBanner;
+  }
 }
 
-export default HomePage; 
+export default HomePage;
