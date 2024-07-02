d82744fc76bc79c6cf0fd4720c320d225cdf3bb2
feroline <anacarolina.rrocha@gmail.com>
2024-07-02 19:42:07 -0300

    Adicionando esqueleto do teste de Elements

---
+++ cypress/e2e/elements.cy.ts
@@ -0,0 +1,32 @@
+import { describe } from "mocha";
+import HomePage from "../pages/HomePage"
+
+beforeEach(() => {
+    cy.visit('/');
+});
+
+describe('Testes da página Elements', () => {
+    describe('Tela inicial', () => {
+
+    });
+
+    describe('Text Box', () => {})
+
+    describe('Check Box', () => {})
+
+    describe('Radio Button', () => {})
+
+    describe('Web Tables', () => {})
+
+    describe('Buttons', () => {})
+
+    describe('Links', () => {})
+
+    describe('Broken Links - Images', () => {})
+
+    describe('Upload and Download', () => {})
+    
+    describe('Dynamic Properties', () => {})
+})
+
+
---
+++ cypress/pages/ElementsPage.ts
@@ -0,0 +1,5 @@
+class ElementsPage {
+
+}
+
+export default ElementsPage
