b9ef0fbdd0a9b718b243bec01a8dcb81d1d5f6ab
feroline <anacarolina.rrocha@gmail.com>
2024-07-04 11:54:16 -0300

    expand Home adicionado

--- cypress/e2e/elements/checkBox.cy.ts
+++ cypress/e2e/elements/checkBox.cy.ts
@@ -1,5 +1,5 @@
-import HomePage from "../../pages/HomePage";
-import ElementsLink from "../../support/Enum/links/Elements";
+import HomePage from '../../pages/HomePage';
+import ElementsLink from '../../support/Enum/links/Elements';
 
 const Home = new HomePage();
 
@@ -10,8 +10,28 @@ beforeEach(() => {
   cy.getItemMenu('Check Box').click();
 });
 
-describe("Testes da tela com Check Box", () => {
-    it("Verifica URL da página", () => {
-        cy.url().should("include", ElementsLink.TEXT_BOX);
+describe('Testes da tela com Check Box', () => {
+    it('Verifica URL da página', () => {
+        cy.url().should('include', ElementsLink.CHECK_BOX);
       });
+
+    describe('Partição Válida', () => {
+        describe('Expand itens do Checkbox', () => {
+            it.only('Expand Home', () => {
+                cy.get('label[for="tree-node-home"]')
+                    .siblings('button.rct-collapse-btn')
+                    .click().then(() => {
+                        cy.get('label[for="tree-node-desktop"]').should('be.visible').and('contain.text', 'Desktop')
+                        cy.get('label[for="tree-node-documents"]').should('be.visible').and('contain.text', 'Documents')
+                        cy.get('label[for="tree-node-downloads"]').should('be.visible').and('contain.text', 'Downloads')
+                    })
+
+            })
+        })
+
+        describe('Collapse itens do Checkbox', () => {})
+    })
+
+    // TODO: implementar partição inválida
+    describe('Partição Inválida', () => {})
 })
