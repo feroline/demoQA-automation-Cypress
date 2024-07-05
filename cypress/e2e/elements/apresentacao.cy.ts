a0f51f1ebf35d00bc91d07de852a36b0e7597c90
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-07-05 11:57:45 -0300

    formatando com prettier e excluindo lixo

--- cypress/e2e/elements/apresentacao.cy.ts
+++ cypress/e2e/elements/apresentacao.cy.ts
@@ -1,32 +1,15 @@
-import HomePage from "../../pages/HomePage";
+import HomePage from '../../pages/HomePage';
 const Home = new HomePage();
 
 beforeEach(() => {
-  cy.visitarToolsQA();
-  Home.elements().click();
+	cy.visitarToolsQA();
+	Home.elements().click();
 });
 
-describe("Testes da página Elements", () => {
-  describe("Tela inicial", () => {
-    it("Verifica mensagem apresentada", () => {
-      cy.verificaMensagemInicial();
-    });
-  });
-
-  // TODO: Implementar testes abaixo em arquivos separados
-  describe("Check Box", () => {});
-
-  describe("Radio Button", () => {});
-
-  describe("Web Tables", () => {});
-
-  describe("Buttons", () => {});
-
-  describe("Links", () => {});
-
-  describe("Broken Links - Images", () => {});
-
-  describe("Upload and Download", () => {});
-
-  describe("Dynamic Properties", () => {});
+describe('Testes da página Elements', () => {
+	describe('Tela inicial', () => {
+		it('Verifica mensagem apresentada', () => {
+			cy.verificaMensagemInicial();
+		});
+	});
 });
