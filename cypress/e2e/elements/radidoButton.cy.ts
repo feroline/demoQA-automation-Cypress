da1a632d28c5030927dc58b322903427e5fe10d2
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-07-10 12:25:40 -0300

    dividindo por partições

--- cypress/e2e/elements/radidoButton.cy.ts
+++ cypress/e2e/elements/radidoButton.cy.ts
@@ -10,20 +10,42 @@ beforeEach(() => {
 	RadioButton.radioBtnMenu().click();
 });
 
-it('Marcar Yes', () => {
-	RadioButton.yesByRadio().should('not.be.checked');
-	RadioButton.yesByLabel().click();
-	RadioButton.yesByRadio().should('be.checked');
+describe('Partição Check - Validar os que foram checkados', () => {
+	it('Marcar Yes', () => {
+		RadioButton.yesByRadio().should('not.be.checked');
+		RadioButton.yesByLabel().click();
+		RadioButton.yesByRadio().should('be.checked');
+	});
+
+	it('Marcar Impressive', () => {
+		RadioButton.impressiveByRadio().should('not.be.checked');
+		RadioButton.impressiveByLabel().click();
+		RadioButton.impressiveByRadio().should('be.checked');
+	});
+
+	it('Marcar No', () => {
+		RadioButton.noByRadio().should('not.be.checked');
+		RadioButton.noByLabel().click();
+		RadioButton.noByRadio().should('not.be.checked').and('be.disabled');
+	});
 });
 
-it('Marcar Impressive', () => {
-	RadioButton.impressiveByRadio().should('not.be.checked');
-	RadioButton.impressiveByLabel().click();
-	RadioButton.impressiveByRadio().should('be.checked');
-});
+describe('Partição Não Check - Validar os que não foram checkados', () => {
+	it('Marcar Yes', () => {
+		RadioButton.yesByLabel().click();
+		RadioButton.impressiveByRadio().should('not.be.checked');
+		RadioButton.noByRadio().should('not.be.checked');
+	});
 
-it('Marcar No', () => {
-	RadioButton.noByRadio().should('not.be.checked');
-	RadioButton.noByLabel().click();
-	RadioButton.noByRadio().should('not.be.checked').and('be.disabled');
+	it('Marcar Impressive', () => {
+		RadioButton.impressiveByLabel().click();
+		RadioButton.yesByLabel().should('not.be.checked');
+		RadioButton.noByRadio().should('not.be.checked');
+	});
+
+	it('Marcar No', () => {
+		RadioButton.noByLabel().click();
+		RadioButton.yesByLabel().should('not.be.checked');
+		RadioButton.impressiveByLabel().should('not.be.checked');
+	});
 });
