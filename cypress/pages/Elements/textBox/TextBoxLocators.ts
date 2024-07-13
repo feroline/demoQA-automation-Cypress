83593aee49fe51c93e7367cecc1c5c26e8820ae3
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-07-13 19:03:16 -0300

    adicionando locators para o TextBoxPage

---
+++ cypress/pages/Elements/textBox/TextBoxLocators.ts
@@ -0,0 +1,13 @@
+const TextBoxLocators = {
+	itemMenu: 'Text Box',
+	usernameInput: '#userName',
+	emailInput: '#userEmail',
+	currentAddressInput: '#currentAddress',
+	permanentAddressInput: '#permanentAddress',
+	submitBtn: '#submit',
+	output: '#output',
+	nameOutout: '#name',
+	emailOutput: '#email',
+};
+
+export default TextBoxLocators;
