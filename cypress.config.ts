689c65b958357a7fe8cb445a1b65e937f24989e9
Ana Carolina - QA - Tester - Automation <anacarolina.rrocha@gmail.com>
2024-07-13 21:29:06 -0300

    atualização final

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "cypress";
-require('dotenv').config()
+import { defineConfig } from 'cypress';
+require('dotenv').config();
 
 export default defineConfig({
-  projectId: process.env.CYPRESS_PROJECT_ID,
-  e2e: {
-    baseUrl: "https://demoqa.com",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	projectId: process.env.CYPRESS_PROJECT_ID,
+	e2e: {
+		baseUrl: 'https://demoqa.com',
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 });
